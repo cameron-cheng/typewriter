b8f86961d4065e98b57e02a77193fe5087524f62
vagrant <Dcameroncheng@gmail.com>
2020-04-07 20:10:37 +0000

    updated typewriter to add new space

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,14 @@
 const sentence = "hello there from lighthouse labs";
-let delay = 0;
-for (const char of sentence) {
+for (let i = 0; i < sentence.length; i++) {
+  let isLast = false;
+  if ( i === sentence.length - 1) {
+    isLast = true;
+  }
   setTimeout(() => {
-    process.stdout.write(char);
-  }, delay += 50) // <= 1s delay to make it noticeable. Can dial it down later.
-  setTimeout(() => {
-    process.stdout.write('\n');
-  }, 2000);
+    process.stdout.write(sentence[i]);
+    if (isLast) {
+      process.stdout.write('\n');
+    }
+  }, 50 * i) // <= 1s delay to make it noticeable. Can dial it down later.
   
-  
-  
-}
+}   
