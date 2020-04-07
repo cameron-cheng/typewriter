a48cf2a0db6672b2236c9ab5a7b96d01bbeb999c
vagrant <Dcameroncheng@gmail.com>
2020-04-07 19:33:59 +0000

    creaTed a type writer functionality for string

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay += 50) // <= 1s delay to make it noticeable. Can dial it down later.
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, 2000);
+  
+  
+  
+}
